8d9e5aab5ec181e13b44a4dbd47117ca899f6724
boombuster2000 <chanuka.williams@outlook.com>
2023-10-30 18:57:39 +0000

    Created playerInput function. printBoard prints new line after printing board.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,30 @@ fun printBoard(board: Array<Array<String>>) {
         for (x in board[y].indices) {
             print(" ${board[y][x]} |")
         }
-        if (y != 2) println("\n---+---+---+---")
+        if (y != 2) println("\n---+---+---+---") else println()
     }
 }
 
+fun getPlayerInput(): Map<String, Int> {
+    while (true) {
+        print(">> ")
+        val playerMoveUnformatted = readln()
+        try {
+            if (playerMoveUnformatted.length != 3 || playerMoveUnformatted[1] != ' ') {
+                println("Input should be (x y)")
+                continue
+            }
+
+            return mapOf("x" to playerMoveUnformatted[0].digitToInt(), "y" to playerMoveUnformatted[2].digitToInt())
+
+        } catch (error: java.lang.IllegalArgumentException) {
+            println("Input should be numbers")
+        } catch (error: StringIndexOutOfBoundsException) {
+            println("Input should be (x y)")
+        }
+    }
+
+}
 fun main() {
     val board = arrayOf(
         arrayOf(" ", " ", " "),
@@ -15,5 +35,6 @@ fun main() {
     )
 
     printBoard(board)
+    getPlayerInput()
 
 }
